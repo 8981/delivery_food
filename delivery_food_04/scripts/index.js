cfb6644a3b45b1ef77db0864efd2581ea0f63196
Artem Prokofiev <artem-prokofiev@yandex.ru>
2025-09-17 23:50:04 +0300

    домашка 4

--- delivery_food_04/scripts/index.js
+++ delivery_food_04/scripts/index.js
@@ -114,7 +114,7 @@ const restFunc = () => {
                                         <img src="./images/icons/star.svg" alt="star"> ${card.rating}
                                     </div>
                                     <div class="products-cart_description-info--price">
-                                        ${card.price}
+                                        ${card.price} ₽
                                     </div>
                                     <div class="products-cart_description-info--group">
                                         ${card.type}
@@ -136,5 +136,111 @@ const restFunc = () => {
     }
 }
 
+const cardRestFunc = () => {
+    const container = document.querySelector('#rest-card_container')
+
+    const restCardArray = [
+        {
+            id: 0,
+            name: 'Ролл угорь стандарт',
+            text: 'Рис, угорь, соус унаги, кунжут, водоросли нори.',
+            price: 250,
+            image: 'good-1.jpg'
+        },
+
+        {
+            id: 1,
+            name: 'Калифорния лосось стандарт',
+            text: 'Рис, лосось, авокадо, огурец, майонез, икра масаго, водоросли нори.',
+            price: 395,
+            image: 'california-standart.jpg'
+        },
+
+        {
+            id: 2,
+            name: 'Окинава стандарт',
+            text: 'Рис, креветка отварная, сыр сливочный, лосось, огурец свежий...',
+            price: 250,
+            image: 'okinava-standart.jpg'
+        },
+
+        {
+            id: 3,
+            name: 'Цезарь маки хl',
+            text: 'Рис, куриная грудка копченая, икра масаго, томат, айсберг, соус цезарь...',
+            price: 250,
+            image: 'cizar-mix.jpg'
+        },
+
+        {
+            id: 4,
+            name: 'Ясай маки стандарт 185 г',
+            text: 'Рис, помидор свежий, перец болгарский, авокадо, огурец, айсберг',
+            price: 250,
+            image: 'yasay-standart.jpg'
+        },
+
+        {
+            id: 5,
+            name: 'Ролл с креветкой стандарт',
+            text: 'Рис, водоросли нори, креветки отварные, сыр сливочный, огурцы',
+            price: 250,
+            image: 'shrimp-standart.jpg'
+        }
+
+    ]
+
+    const loading = () => {
+        container.innerHTML = '<p style="width: 100%; text-align: center;">Загрузка</p>'
+    }
+
+    const randerCartRests = (array) => {
+        container.innerHTML = ''
+
+        array.forEach((card) => {
+            container.insertAdjacentHTML('beforeend', `
+                <div class="products-cart">
+                    <div class="products-cart_image">
+                        <img src="./images/goods/${card.image}" alt="rest-1">
+                    </div>
+                    <div class="products-cart_description">
+                        <div class="products-cart_description-row">
+                            <h5 class="products-cart_description--name">
+                                ${card.name}
+                            </h5>
+                        </div>
+                        <div class="products-cart_description-row">
+                            <p class="products-cart_description--text">
+                                ${card.text}
+                            </p>
+                        </div>
+                        <div class="products-cart_description-row">
+                            <div class="products-cart_description-controls">
+                                <button class="btn btn-primary">
+                                    В корзину
+                                    <img src="./images/icons/shopping-cart-white.svg" alt="shopping-cart-white">
+                                </button>
+
+                                <span class="products-cart_description-controls--price">
+                                    ${card.price} ₽
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `)
+        })
+    }
+
+    if (container) {
+        loading()
+    
+        setTimeout(() => {
+            randerCartRests(restCardArray)
+        }, 1000)
+    }
+}
+
 modalFunc()
 restFunc()
+cardRestFunc()
