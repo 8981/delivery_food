0b2638278cb4222c2a62e5370faf521aed0c35e8
Artem Prokofiev <artem-prokofiev@yandex.ru>
2025-09-19 00:07:25 +0300

    домашка 5

---
+++ delivery-vue/src/constants/rests.js
@@ -0,0 +1,61 @@
+export const restArray = [
+  {
+    id: 0,
+    title: "Пицца плюс",
+    time: 50,
+    rating: 4.5,
+    price: 900,
+    type: "Пицца",
+    image: "pizza-plus.jpg",
+  },
+
+  {
+    id: 1,
+    title: "Тануки",
+    time: 50,
+    rating: 4.5,
+    price: 900,
+    type: "Пицца",
+    image: "tanooki.jpg",
+  },
+
+  {
+    id: 2,
+    title: "FoodBand",
+    time: 50,
+    rating: 4.5,
+    price: 900,
+    type: "Пицца",
+    image: "food-band.jpg",
+  },
+
+  {
+    id: 3,
+    title: "Жадина-пицца",
+    time: 50,
+    rating: 4.5,
+    price: 900,
+    type: "Пицца",
+    image: "greedy-pizza.jpg",
+  },
+
+  {
+    id: 4,
+    title: "Точка еды",
+    time: 50,
+    rating: 4.5,
+    price: 900,
+    type: "Пицца",
+    image: "foodpoint.jpg",
+  },
+
+  {
+    id: 5,
+    title: "PizzaBurger",
+    time: 50,
+    rating: 4.5,
+    price: 900,
+    type: "Пицца",
+    image: "pizza-burger.jpg",
+  },
+];
---
+++ delivery-vue/src/views/ProductView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <section class="products">
+      <div class="container">
+        <div class="products-wrapper" id="rest-card_container">
+          <div class="products-cart" v-for="rest in restCardArray" :key="rest.id">
+            <div class="products-cart_image">
+              <img :src="getUrl(rest.image)" alt="rest-1" />
+            </div>
+            <div class="products-cart_description">
+              <div class="products-cart_description-row">
+                <h5 class="products-cart_description--name">
+                  {{rest.name}}
+                </h5>
+              </div>
+              <div class="products-cart_description-row">
+                <p class="products-cart_description--text">
+                  {{rest.text}}
+                </p>
+              </div>
+              <div class="products-cart_description-row">
+                <div class="products-cart_description-controls">
+                  <button class="btn btn-primary">
+                    В корзину
+                    <img
+                      src="../assets/images/icons/shopping-cart-white.svg"
+                      alt="shopping-cart-white"
+                    />
+                  </button>
+
+                  <span class="products-cart_description-controls--price">
+                    {{rest.price}} ₽
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script setup>
+import { restCardArray } from '../constants/cartRests';
+const getUrl = (name) => {
+  return new URL(`../assets/images/goods/${name}`, import.meta.url);
+};
+</script>
